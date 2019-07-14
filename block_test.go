dca64af11746ff902ba874de582a359f72fad322
- <->
2019-07-14 14:52:32 +0800

    .

--- block_test.go
+++ block_test.go
@@ -1,21 +1,21 @@
 package block
 
 import (
+	"crypto/rand"
 	"fmt"
+	"github.com/u6du/block/xor"
+	"github.com/u6du/highwayhash"
 	"testing"
 	"time"
-
-	"github.com/u6du/highwayhash"
-
-	"github.com/u6du/block/xor"
 )
 
 func TestBlock(t *testing.T) {
 
 	var msg [32]byte
+	rand.Read(msg[:])
 
 	begin := uint64(time.Now().UnixNano())
-	for count := uint64(1); count <= 10; count++ {
+	for count := uint64(1); count <= 100; count++ {
 		salt := Begin0MoreThan(msg, 23)
 		cost := (uint64(time.Now().UnixNano()) - begin) / uint64(time.Millisecond) / count
 		fmt.Printf("MsgSaltLeadingZeroCount %d\n", MsgSaltLeadingZeroCount(msg, salt[:]))
