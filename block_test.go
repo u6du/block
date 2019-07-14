af828f2b76da950f0b5ca141b6c2098b0f31fdc9
- <->
2019-07-14 14:50:16 +0800

    .

--- block_test.go
+++ block_test.go
@@ -1,6 +1,7 @@
 package block
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
@@ -15,13 +16,13 @@ func TestBlock(t *testing.T) {
 
 	begin := uint64(time.Now().UnixNano())
 	for count := uint64(1); count <= 10; count++ {
-		salt := Begin0MoreThan(msg, 22)
+		salt := Begin0MoreThan(msg, 23)
 		cost := (uint64(time.Now().UnixNano()) - begin) / uint64(time.Millisecond) / count
-		t.Logf("MsgSaltLeadingZeroCount %d\n", MsgSaltLeadingZeroCount(msg, salt[:]))
+		fmt.Printf("MsgSaltLeadingZeroCount %d\n", MsgSaltLeadingZeroCount(msg, salt[:]))
 
 		xor.Copy32(&salt, &msg)
-		t.Logf("%dms %x\n", cost, highwayhash.Zero.Sum(append(msg[:], salt[:]...)))
-		t.Logf("%x %x\n\n", msg, salt)
+		fmt.Printf("%dms %x\n", cost, highwayhash.Zero.Sum(append(msg[:], salt[:]...)))
+		fmt.Printf("%x %x\n\n", msg, salt)
 		Next(&msg)
 	}
 }
